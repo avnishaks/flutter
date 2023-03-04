7c97da76eab8e08322d551a78d0ef9cded5db156
avnishaks <avnishgolu927@gmail.com>
2023-03-04 11:58:53 +0530

    Different Type of the Widget in Scaffold

--- bootcamp_app/lib/main.dart
+++ bootcamp_app/lib/main.dart
@@ -12,19 +12,35 @@ runApp-> root widget where all the component of the widget will come
 
 
 1. Material App define the certain theme called Material Theme and also provide the direction.
-2. Scaffold does : Basically helps us to make the new page
+2. Scaffold does : Basically helps us to make the new page (give the width and height)
+Scaffold Widget create different general widget use in the Mobile Application Development 
+Each of the Widget conatins the different properties which it hold to show the use case of mobile 
+Development.
+
+Eg : AppBar class different number of the properties used , we can google it and explore it.
+
+
 
 */
 
 import 'package:flutter/material.dart';
 
 main() {
+  void myfunction(){
+    print("AKS");
+  }
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
+       leading: Text("Leader of the Board"),
         title: Text("My Personal Blog"),
       ),
       drawer:Text("Swipe Feature"),
+      floatingActionButton:FloatingActionButton(
+        onPressed:myfunction,
+        child: Text("+"),
+      ),
+      body: Text("Body of the our Application"),
     ),
   ));
 }
