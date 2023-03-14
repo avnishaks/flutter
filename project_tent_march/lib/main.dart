5076d3ea0dc07f85ecca64f204808e871b566a44
avnishaks <avnishgolu927@gmail.com>
2023-03-14 21:55:11 +0530

    widget column

--- project_tent_march/lib/main.dart
+++ project_tent_march/lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 List<Color> myColors = [
   Colors.red,
+  Colors.yellow,
+  Colors.blue,
   Colors.brown,
   Colors.green
 ];
 
-
 void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: myHome(),
     ));
 
@@ -21,84 +22,9 @@ class myHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          Container(
-             color: Colors.red,
-             child:Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                
-                TextButton(onPressed: (){}, child:Text('Monday')),
-                TextButton(onPressed: (){}, child: Text('27/11/2000'))
-              ],),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Lifeline',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              )
-              
-             ],)
-          ),
-
-          Container(
-             color: Colors.red,
-             child:Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                
-                TextButton(onPressed: (){}, child:Text('Monday')),
-                TextButton(onPressed: (){}, child: Text('27/11/2000'))
-              ],),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Lifeline',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              )
-              
-             ],)
-          ),
-
-          Container(
-             color: Colors.red,
-             child:Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                
-                TextButton(onPressed: (){}, child:Text('Monday')),
-                TextButton(onPressed: (){}, child: Text('27/11/2000'))
-              ],),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Lifeline',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              )
-              
-             ],)
-          ),
-
-         // ...myColors.map((color) => getMyContainer(color)).toList()
-
-
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          ...myColors.map((color) => getMyContainer(color)).toList(),
+          ElevatedButton(onPressed: () => null, child: Text("Button"))
         ]),
       ),
     );
@@ -106,7 +32,7 @@ class myHome extends StatelessWidget {
 
   Widget getMyContainer(Color color) {
     return Container(
-      height: 50,
+      width: 50,
       color: color,
     );
   }
