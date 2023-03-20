f364d704bfd07273248ccf0796b2791ecc648b5d
avnishaks <avnishgolu927@gmail.com>
2023-03-20 18:31:25 +0530

    Implement the Model class Enhance function and App error handling

--- chatgpt_app/lib/constants/api_consts.dart
+++ chatgpt_app/lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
--- chatgpt_app/lib/services/api_service.dart
+++ chatgpt_app/lib/services/api_service.dart
@@ -1,26 +1,35 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
+import 'package:chatgpt_app/models/models_model.dart';
 import 'package:http/http.dart' as http;
 
 import '../constants/api_consts.dart';
 
 class ApiService {
-  static Future<void> getModels() async {
+  static Future<List<ModelsModel>> getModels() async {
     try {
       var response = await http.get(
         Uri.parse("$BASE_URL/models"),
         headers: {'Authorization': 'Bearer $API_KEY'},
       );
-      
+
       Map jsonResponse = jsonDecode(response.body);
       if (jsonResponse['error'] != null) {
-        print("jsonResponse['error'] ${jsonResponse['error']["message"]}");
+        //print("jsonResponse['error'] ${jsonResponse['error']["message"]}");
         throw HttpException(jsonResponse['error']["message"]);
       }
-      print("jsonResponse $jsonResponse");
+      //print("jsonResponse $jsonResponse");
+      List temp=[];
+      for(var value in jsonResponse["data"]){
+        temp.add(value);
+        log("temp ${value["id"]}");
+      }
+      return ModelsModel.modelsFromSnapshot(temp);
     } catch (error) {
-      print("error $error");
+      log("error $error");
+      rethrow;
     }
   }
 }
